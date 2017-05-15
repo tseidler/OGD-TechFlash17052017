c4207cf4d4cd43a84580b1fe4adc7de25a8d0bce
tseidler <terry@salteh.net>
2017-05-15 21:50:02 +0200

    Add Particle, random colour

--- vuurwerk.js
+++ vuurwerk.js
@@ -3,13 +3,22 @@
   let canvas = document.getElementById("canvas");
   let context = canvas.getContext("2d");
   let lastUpdate = 0;
+  let particles = [];
+  const COLOURS = ['white', 'green', 'yellow', 'orange', 'pink', 'blue', 'brown', 'red', 'magenta', 'purple'];
 
-  window.addEventListener("resize", resizeCanvas, false);
+  window.addEventListener("resize", resizeCanvas);
+  canvas.addEventListener("click", addParticle);
 
   function resizeCanvas(oEvent) {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     draw();
+  }
+
+  function addParticle(oEvent) {
+    let colour = COLOURS[Math.floor(Math.random() * COLOURS.length)];
+    let particle = new Particle(oEvent.clientX, oEvent.clientY, 5, colour);
+    particles.push(particle);
   }
 
   function update(timestamp) {
@@ -25,7 +34,23 @@
 
   function draw(dTime) {
     context.clearRect(0, 0, canvas.width, canvas.height);
+
+    particles.forEach(particle => particle.draw(context));
   }
   resizeCanvas();
   window.requestAnimationFrame(timestamp => update(timestamp));
+
+  class Particle {
+    constructor(x, y, size, colour) {
+      this.x = x;
+      this.y = y;
+      this.size =  size;
+      this.colour = colour;
+    }
+
+    draw(context) {
+      context.fillStyle = this.colour;
+      context.fillRect(this.x, this.y, this.size, this.size);
+    }
+  }
 })();
